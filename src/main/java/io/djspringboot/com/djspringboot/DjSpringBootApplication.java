e3f184e2d776bcc1b8c4033a796d20726c0b53fe
dhanajeyanAosta <dhanajeyan@gmail.com>
2019-06-13 20:22:28 +0530

    Swagger ui included

--- src/main/java/io/djspringboot/com/djspringboot/DjSpringBootApplication.java
+++ src/main/java/io/djspringboot/com/djspringboot/DjSpringBootApplication.java
@@ -2,12 +2,24 @@ package io.djspringboot.com.djspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DjSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DjSpringBootApplication.class, args);
 	}
 
+	@Bean
+	public Docket EmployeeApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("io.djspringboot.com.djspringboot")).build();
+	}
 }
